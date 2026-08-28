095f764ee8f5830911ff5b1939f608232552e96a
PEDRO HENRIQUE LUNEDO DE ANDRADE <pedro_hl_andrade@estudante.sesisenai.org.br>
2026-08-28 11:57:16 -0300

    java joptionpane

---
+++ 202/java/exr04_JOptionPane/src/exr04_joptionpane/Exr04_JOptionPane.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exr04_joptionpane;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author pedro_hl_andrade
+ */
+public class Exr04_JOptionPane {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        JOptionPane.showMessageDialog(null, "Olá! Seja bem-vindo!");
+        
+        String nome = JOptionPane.showInputDialog("Digite seu nome: ");
+        
+        int idade = Integer.parseInt(JOptionPane.showInputDialog("Digite sua idade: ") );
+        
+        // abre um diálogo com opções de SIM e NÃO
+        
+       int resposta = JOptionPane.showConfirmDialog(
+        null,
+        "Você deseja continuar?", // Pergunta apresentada ao usuário
+        "Confirmação", // Título da caixa de diálogo
+        JOptionPane.YES_NO_OPTION // Define que serão exibidos os botões SIM e NÃO
+        );
+       
+       if (resposta == JOptionPane.YES_OPTION) {
+        JOptionPane.showMessageDialog(null, "Você escolheu SIM!");}
+}
+}
